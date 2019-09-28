6d083eeec0e4b9ded30e0483f9adacda54ace7f9
thecodeissyx <syirilrakhulhaqim@gmail.com>
2019-09-29 01:19:33 +0700

    fix input formula js and fix html

--- formula.js
+++ formula.js
@@ -7,17 +7,26 @@ function getInput() {
   }
 }
 
+function resInput() {
+  document.getElementById("male").value = "";
+  document.getElementById("female").value = "";
+}
+
 function jodohin() {
   getInput();
   if (getInput() == "kosong") {
     alert("Input tidak boleh kosong!");
   } else {
+    var male = document.getElementById("male").value;
+    var female = document.getElementById("female").value;
     var batas = 100;
     var hasil = Math.floor(Math.random() * batas);
     if (hasil === 0) {
       document.getElementById("result").innerHTML = "Maaf kamu bukan jodoh dia";
     } else {
-      document.getElementById("result").innerHTML = "Hasil : " + hasil + " %";
+      document.getElementById("result").innerHTML =
+        male + " dan " + female + " hasilnya : " + hasil + " %";
     }
+    resInput();
   }
 }
